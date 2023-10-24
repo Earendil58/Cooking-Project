b84e023fb82c3d4c853e4d1147155ee1591517b7
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-10-24 16:54:51 -0300

    template driven approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingService } from '../shopping.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,18 +12,14 @@ export class ShoppingEditComponent implements OnInit {
 
 
 
-  @ViewChild('nameInput') nameInput: ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef ;
-
   constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-    const ingName = this.nameInput.nativeElement.value;
-    const ingAmount = this.amountInput.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onClick(form: NgForm){
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingService.addIngredient(newIngredient);
   }
 
