e36263813542b1b889199d09178d2c89ee5bf68d
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-10-26 15:55:36 -0300

    init of the reactive driven approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { AppRouting } from './app-routing.module';
@@ -35,7 +35,8 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
-    AppRouting
+    AppRouting,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
